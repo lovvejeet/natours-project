5c9c1d8c36e233ca78cac385d05a1e1fc16286cc
lovejeet <lovvejeetsandhu@gmail.com>
2023-06-28 11:36:13 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+<link href="https://fonts.googleapis.com/css?family=Lato: 100, 300, 400,700,900" rel="stylesheet">
+
+<header class="header">
+  <div class="header__logo-box">
+    <img src="../src/assets/logo-white.png" alt="LOGO" class="header__logo">
+  </div>
+
+  <div class="header__text-box">
+    <h1 class="heading-primary">
+       <span class="heading-primary--main">Outdoors</span>
+       <span class="heading-primary--sub">is where life happens</span>
+    </h1>
+    <a href="#" class="btn btn--white btn--animated">Discover our tours</a>
+  </div>
+ </header>
+
+ <main>
+   <section class="section-about">
+    <div class="u-center-text u-margin-bottom-big">
+      <h2 class="heading-secondary">Exciting tours for adventurous people</h2>
+    </div>
+    <div id="row-size" class="row-col">
+      <div class="col-1-of-2">
+        <h3 class="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
+        <p class="paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio magni deserunt explicabo. Iste consequuntur libero commodi eos, eveniet labore ut.</p>
+        <h3 class="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
+        <p class="paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio magni deserunt explicabo. Iste consequuntur libero commodi eos, eveniet labore ut.</p>
+      
+      <a href="#" class="btn-text">Learn more &rarr;</a>
+      
+       </div>
+      <div id="row-size" class="row-col">
+      <div class="col-1-of-2">
+        <div class="composition">
+          <img src="..\src\assets\nat-1-large.jpg" alt="photo 1" class="composition__photo composition__photo__p1">
+          <img src="..\src\assets\nat-2-large.jpg" alt="photo 2" class="composition__photo composition__photo__p2">
+          <img src="..\src\assets\nat-3-large.jpg" alt="photo 3" class="composition__photo composition__photo__p3">
+        </div>
+      </div>
+    </div>
+    </div>
+    </section>
+ </main>
+</template>
+
+
+<style>
+@import 'style.css';
+</style>
+
+
+
