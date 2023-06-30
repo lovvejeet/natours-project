40913c248caf9beb44f18819fff467d0c146dab7
lovejeet <lovvejeetsandhu@gmail.com>
2023-06-30 15:35:33 +0530

    new changes

--- src/App.vue
+++ src/App.vue
@@ -83,42 +83,109 @@
     </div>
     <div id="row-size" class="row-col">
       <div class="col-1-of-3">
-        <div class="card">
-          <div class="card__side card__side--front">
-            <div class="card__picture card__picture--1">
+        <div class="flip">
+          <div class="flip__side flip__side--front">
+            <div class="flip__picture flip__picture--1">
               &nbsp;
             </div>
-            <h4 class="card__heading">
-              <span class="card__heading-span card__heading-span--1"></span>
-                  The Sea Explorer
+            <h4 class="flip__heading">
+              <span class="flip__heading-span flip__heading-span--1">The Sea Explorer</span>
+                  
             </h4>
-            <div class="card__details">
-                  details
+            <div class="flip__details">
+                                    <ul>
+                                        <li>3 day tours</li>
+                                        <li>Up to 30 people</li>
+                                        <li>2 tour guides</li>
+                                        <li>Sleep in cozy hotels</li>
+                                        <li>Difficulty: easy</li>
+                                    </ul>
             </div>
            </div>
-           <div class="card__side card__side--back card__side--back-1">
-            
+           <div class="flip__side flip__side--back flip__side--back-1">
+            <div class="flip__cta">
+                                    <div class="flip__price-box">
+                                        <p class="flip__price-only">Only</p>
+                                        <p class="flip__price-value">₹4999</p>
+                                    </div>
+                                    <a href="#popup" class="btn btn--white">Book now!</a>
+                                </div>
            </div>
            
           </div>
          </div>
          <div class="col-1-of-3">
-          <div class="card">
-           <div class="card__side">
-            
+          <div class="flip">
+          <div class="flip__side flip__side--front">
+            <div class="flip__picture flip__picture--2">
+              &nbsp;
+            </div>
+            <h4 class="flip__heading">
+              <span class="flip__heading-span flip__heading-span--2">The Forest Adventurer</span>
+                  
+            </h4>
+            <div class="flip__details">
+              <ul>
+                                        <li>7 day tours</li>
+                                        <li>Up to 40 people</li>
+                                        <li>6 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: medium</li>
+                                    </ul>
+            </div>
            </div>
+           <div class="flip__side flip__side--back flip__side--back-2">
+            <div class="flip__cta">
+                                    <div class="flip__price-box">
+                                        <p class="flip__price-only">Only</p>
+                                        <p class="flip__price-value">₹19999</p>
+                                    </div>
+                                    <a href="#popup" class="btn btn--white">Book now!</a>
+                                </div>
+           </div>
+           
           </div>
+          
           
          </div>
     
-    <div class="col-1-of-3">
-      <div class="card">
-           <div class="card__side">
-            
+         <div class="col-1-of-3">
+          <div class="flip">
+          <div class="flip__side flip__side--front">
+            <div class="flip__picture flip__picture--3">
+              &nbsp;
+            </div>
+            <h4 class="flip__heading">
+              <span class="flip__heading-span flip__heading-span--3">The Snow Adventurer</span>
+                  
+            </h4>
+            <div class="flip__details">
+              <ul>
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+            </div>
            </div>
+           <div class="flip__side flip__side--back flip__side--back-3">
+            <div class="flip__cta">
+                                    <div class="flip__price-box">
+                                        <p class="flip__price-only">Only</p>
+                                        <p class="flip__price-value">₹29999</p>
+                                    </div>
+                                    <a href="#popup" class="btn btn--white">Book now!</a>
+                                </div>
+           </div>
+           
           </div>
+          
          </div>
         </div>
+<div class="u-center-text u-margin-top-big">
+          <a href="#" class="btn btn--green" style="color:white;">Discover all tours</a>
+    </div>
 </section>
  </main>
 </template>
